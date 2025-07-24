480f7f1b2e21ca8ab21cff35244980fa1546e13a
Vladislav Kutas <kutas.uladzislau@gmail.com>
2025-07-24 16:02:16 +0300

    add pipe

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+file_posfix=""
+if [ -n "$1" ]; then
+    file_posfix="-${1}"
+fi
+
+zip -r -FS ../polyglotter.zip * --exclude '*.git*' '*.sh' 
+mv ../polyglotter.zip "../polyglotter${file_posfix}.xpi"
