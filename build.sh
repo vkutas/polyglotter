dd83af686de7999cbdb03479e78c52e516ad2c80
Vladislav Kutas <kutas.uladzislau@gmail.com>
2025-07-24 23:33:56 +0300

    debug

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 set -ex
-
-file_posfix=""
-if [ -n "$1" ]; then
-    file_posfix="-${1}"
-fi
-
-# zip -r -FS ../polyglotter.zip * --exclude '*.git*' '*.sh' 
-# mv ../polyglotter.zip "../polyglotter${file_posfix}.xpi"
-
-
-zip -r -FS ../polyglotter${file_posfix}.xpi * --exclude '*.git*' '*.sh'
-# mv ../polyglotter.zip ../polyglotter.xpi
+zip -r -FS ../polyglotter.xpi * --exclude '*.git*' '*.sh' '.gitignore'
